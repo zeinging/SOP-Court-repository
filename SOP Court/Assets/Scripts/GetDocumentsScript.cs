11e80170dace98d57d94c9346ce5f2d6c9bee1e1
Shawn Peery <shawn.w8465@gmail.com>
2023-07-27 20:14:47 -0600

    Updates scripts

--- SOP Court/Assets/Scripts/GetDocumentsScript.cs
+++ SOP Court/Assets/Scripts/GetDocumentsScript.cs
@@ -1,16 +1,20 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using UnityEngine;
 
 public class GetDocumentsScript : MonoBehaviour
 {
 
     private string myDocumentsFolderPath;
-    
+
     public string[] FoldersInDocumentsFolder;
 
-    public List<string> 
+    private XElement witnessStatement;
+
+
+    public List<string>
     FilesPathCase1Folder,
     FilesPathCase2Folder,
     FilesPathCase3Folder,
@@ -23,33 +27,64 @@ public class GetDocumentsScript : MonoBehaviour
     void Awake()
     {
         myDocumentsFolderPath = Application.dataPath + "/DocumentCases/";
-        GetCaseFolders();
-        GetFilesInFolder(FoldersInDocumentsFolder[0], FilesPathCase1Folder);
-        GetFilesInFolder(FoldersInDocumentsFolder[1], FilesPathCase2Folder);
-        GetFilesInFolder(FoldersInDocumentsFolder[2], FilesPathCase3Folder);
-        GetFilesInFolder(FoldersInDocumentsFolder[3], FilesPathCase4Folder);
-        GetFilesInFolder(FoldersInDocumentsFolder[4], FilesPathCase5Folder);
+
+        string xmlFileName = myDocumentsFolderPath + "Case1/CrossExamination.xml";
+
+        if (!File.Exists(xmlFileName))
+        {
+            Debug.Log("Target File Doesn't Exist!");
+            return;
+        }
+
+        witnessStatement = XElement.Load(xmlFileName);
+        IEnumerable<string> items =
+            from statement in witnessStatement.Elements()
+            select statement.Element("Text").Value + "";
+
+
+
+        Debug.Log("Showing Results:");
+
+        foreach (string item in items)
+        {
+
+            Debug.Log(item);
+        }
+
+
+
+
+        //GetCaseFolders();
+        //GetFilesInFolder(FoldersInDocumentsFolder[0], FilesPathCase1Folder);
+        //GetFilesInFolder(FoldersInDocumentsFolder[1], FilesPathCase2Folder);
+        //GetFilesInFolder(FoldersInDocumentsFolder[2], FilesPathCase3Folder);
+        //GetFilesInFolder(FoldersInDocumentsFolder[3], FilesPathCase4Folder);
+        //GetFilesInFolder(FoldersInDocumentsFolder[4], FilesPathCase5Folder);
     }
 
-    public void GetCaseFolders(){
+    public void GetCaseFolders()
+    {
         //namesArray = File.ReadAllLines(myFilePathCase1);
         //filesInCaseFolder = Directory.GetFiles(myFilePathCase1);
         FoldersInDocumentsFolder = Directory.GetFiles(myDocumentsFolderPath);
 
-        for(int s = 0; s < FoldersInDocumentsFolder.Length; s++){
+        for (int s = 0; s < FoldersInDocumentsFolder.Length; s++)
+        {
             int Sstart = FoldersInDocumentsFolder[s].IndexOf(".");
             FoldersInDocumentsFolder[s] = FoldersInDocumentsFolder[s].Remove(Sstart, 5);
             FoldersInDocumentsFolder[s] += "/";
         }
-
     }
 
-    public void GetFilesInFolder(string casePath, List<string> Files){
+    public void GetFilesInFolder(string casePath, List<string> Files)
+    {
 
-        string [] AllFiles = Directory.GetFiles(casePath);
+        string[] AllFiles = Directory.GetFiles(casePath);
 
-        for(int i = 0; i < AllFiles.Length; i++){
-            if(!AllFiles[i].Contains(".meta")){//don't add to list if it's a meta file
+        for (int i = 0; i < AllFiles.Length; i++)
+        {
+            if (!AllFiles[i].Contains(".meta"))
+            {//don't add to list if it's a meta file
                 //FilesInCase1Folder.Add(AllFiles[i]);
                 Files.Add(AllFiles[i]);
             }
@@ -57,14 +92,11 @@ public class GetDocumentsScript : MonoBehaviour
 
     }
 
-    public string[] GetTextFromFileTest(int s){
+    public string[] GetTextFromFileTest()
+    {
         //Debug.Log(FilesPathCase1Folder[0]);
-        if( s < FilesPathCase1Folder.Count){
-        string[] Dialogue = File.ReadAllLines(FilesPathCase1Folder[s]);
+        string[] Dialogue = File.ReadAllLines(FilesPathCase1Folder[0]);
         return Dialogue;
-        }
-        return null;
-        //return Dialogue;
         //string[] Dialogue = new string[1];
         //Dialogue[0] = "testing this fly is really anoying!";
     }
@@ -72,6 +104,6 @@ public class GetDocumentsScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }

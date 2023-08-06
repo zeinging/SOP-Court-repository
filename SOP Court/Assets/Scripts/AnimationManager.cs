8121e724a9c8ebb18f1014cab88cc7db4486f90f
Shawn Peery <shawn.w8465@gmail.com>
2023-08-06 17:51:21 -0600

    Adds bagelman stuff

--- SOP Court/Assets/Scripts/AnimationManager.cs
+++ SOP Court/Assets/Scripts/AnimationManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
@@ -8,8 +6,25 @@ public class AnimationManager : MonoBehaviour
     public Animator WitnessAnim, JudgeAnim, DefenceAnim, ProsectorAnim;
 
     // Start is called before the first frame update
+
+    public static AnimationManager instance;
+
+
+
+
     void Start()
     {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
         WitnessAnim.runtimeAnimatorController = SuspectProfileScript.instance.CurrentSuspectData.myAnim;
         //WitnessAnim.runtimeAnimatorController = null;
     }
